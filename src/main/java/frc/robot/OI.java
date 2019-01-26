aaa3c291773afef589c754c6cafa3d5373e066e1
Jamesni2003 <2667151484@qq.com>
2019-01-26 15:48:39 -0500

    oi for intake

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,17 +2,21 @@ package frc.robot;
 
 import frc.robot.Motors.driveTrain;
 import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.Motors.Intake;
 
 public class OI {
     private Joystick moving = new Joystick(0);
     private driveTrain drive = driveTrain.getInstance();
-   
+    private driveTrain ball_and_wrist = Intake.getInstance();
+
     public OI(){
 
     }
     public void driveRobot(){
         drive.SetSpeed(moving.getRawAxis(5), moving.getRawAxis(1));
     }
+    public void inTake(){
 
+    }
 
 }
