40823a1ab2cb8d825cf7b8701d0ca0af344ed5c6
Jamesni2003 <32179716+Jamesni2003@users.noreply.github.com>
2019-02-05 14:46:00 -0500

    Add files via upload
    gyro

---
+++ src/main/java/frc/robot/Sensors/Gyro.java
@@ -0,0 +1,28 @@
+package frc.robot.Sensors;
+
+
+
+//todo import the analog gyro
+
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+
+
+public class Gyro{
+    double error = 5;
+    
+    //todo tell robot to go to 90 degrees or other angle. when use press a button
+    //use if statement 
+    //reset gyro every time, before going to angle
+    // error = 5 , gyro will not be on the extact angle it will be so you need a error varible.
+
+
+
+
+}
