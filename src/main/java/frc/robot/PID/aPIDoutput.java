4889645b466a73f2eebbaa17e3789516bec381cf
Jamesni2003 <32179716+Jamesni2003@users.noreply.github.com>
2019-02-12 20:30:05 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/PID/aPIDoutput.java
@@ -0,0 +1,17 @@
+package frc.robot.PID;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+import frc.robot.Motors.Arm;
+
+public class aPIDoutput implements PIDOutput{
+    Arm arm;
+    public aPIDoutput(Arm arm){
+        this.arm = arm;
+    }
+
+    public void pidWrite(double speed){
+        arm.SetSpeed(speed);
+    }
+}
+
+
